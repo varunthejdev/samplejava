c3f245ab460bcc1d583fc042b344b85fd13b9b6a
roybhaskar9 <bhaskar@study9.com>
2017-03-13 18:11:10 +0100

    Create seleniumtest.py

---
+++ seleniumtest.py
@@ -0,0 +1,18 @@
+import sys
+from pyvirtualdisplay import Display
+from selenium import webdriver
+
+display = Display(visible=0, size=(800, 600))
+display.start()
+
+browser = webdriver.Firefox()
+browser.get('http://192.168.33.21:8080/dev/')
+print browser.title
+var1 = browser.title
+print var1
+if "Hello Java2" in var1:
+    print "Test Successful"
+else:
+    raise Exception("Test Failed")
+
+browser.close()
